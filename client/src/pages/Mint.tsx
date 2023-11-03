a074b9c13a1b97af7253bd405e1af269a2dbb4a8
0xZurvan <75655454+0xZurvan@users.noreply.github.com>
2023-11-03 00:33:12 -0400

    Network UI update

--- client/src/pages/Mint.tsx
+++ client/src/pages/Mint.tsx
@@ -2,21 +2,19 @@ import Button from "../components/Button";
 import { useAccount, useNetwork } from "wagmi";
 import ConnectWallet from "../components/ConnectWallet";
 import { useNetworkStore } from "../stores/useNetworkStore";
-import { useShallow } from 'zustand/react/shallow'
+import { useShallow } from "zustand/react/shallow";
+import polygon from "../assets/polygon.svg";
 
 export default function Mint() {
   const { isConnected } = useAccount();
   const { chain } = useNetwork();
-  const [toggleModal] =
-    useNetworkStore(
-      useShallow((state) => [
-        state.toggleModal,
-      ])
-    );
+  const [toggleModal] = useNetworkStore(
+    useShallow((state) => [state.toggleModal])
+  );
 
   const openModal = () => {
-    toggleModal(true)
-  }
+    toggleModal(true);
+  };
   return (
     <div className="flex flex-col items-center space-y-10 bg-gray-dark w-[min(44vw)] mx-auto p-8 rounded-lg">
       <h1 className="text-white text-3xl font-ibm font-bold text-center">
@@ -27,8 +25,15 @@ export default function Mint() {
         <h2 className="text-white text-lg font-ibm font-medium">
           SELECT CHAIN:
         </h2>
-        <button onClick={openModal} className="w-full bg-gray-dark border-2 border-gray-medium text-start font-ibm font-semibold hover:text-opacity-80 text-gray-light p-4 rounded-lg">
-          NETWORK: { chain?.name }
+        <button
+          onClick={openModal}
+          className="w-full flex flex-row justify-start items-center gap-8 bg-gray-dark border-2 border-gray-medium text-start font-ibm font-semibold hover:text-opacity-80 text-gray-light p-4 rounded-lg"
+        >
+          <img src={polygon} alt="" />
+          <p className="flex flex-col">
+            <span className="font-ibm font-light text-xl">NETWORK</span> 
+            <span>{chain?.name}</span>
+          </p>
         </button>
       </div>
 
@@ -43,7 +48,7 @@ export default function Mint() {
         {isConnected ? (
           <Button title="MINT" className="w-full text-xl" onClick={() => {}} />
         ) : (
-          <ConnectWallet className="w-full"/>
+          <ConnectWallet className="w-full" />
         )}
       </div>
     </div>
